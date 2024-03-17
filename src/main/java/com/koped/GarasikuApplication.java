5384d2342a579ecde92ee9c77dcf34eb592b18fc
AdamMoh <xlkartu12345@gmail.com>
2024-03-17 17:39:29 +0700

    initial push

---
+++ src/main/java/com/koped/GarasikuApplication.java
@@ -0,0 +1,13 @@
+package com.koped;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GarasikuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GarasikuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koped/GarasikuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koped;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarasikuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
