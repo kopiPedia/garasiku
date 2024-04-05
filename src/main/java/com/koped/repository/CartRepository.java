ebd1a45548fcef3c86cf0d1bcfc8d8dd550070ad
ardhika23 <ardhika@gmail.com>
2024-04-05 19:50:16 +0700

    add CartRepository

--- src/main/java/com/koped/repository/CartRepository.java
+++ src/main/java/com/koped/repository/CartRepository.java
@@ -1,4 +1,28 @@
 package com.koped.repository;
 
+import com.koped.model.Cart;
+import com.koped.model.User;
+import com.koped.model.Product;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.UUID;
+
+
 public class CartRepository {
+    public Cart create(Cart cart) {
+    }
+
+    public void deleteByUserAndProduct(int id) {
+        carts.remove(id);
+    }
+
+    public Cart findByUserAndProduct(int id, int quantity) {
+    }
+
+    public void update(int id, Cart cart) {
+    }
+
+    public Iterator<Cart> findAllByUser(User user) {
+    }
 }
