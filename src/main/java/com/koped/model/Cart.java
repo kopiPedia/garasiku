faf3ce8823193b6dc9d90fe45d6ca45c91aac3e1
ardhika23 <ardhika@gmail.com>
2024-04-05 17:32:57 +0700

    [RED] add test for model

--- src/main/java/com/koped/model/Cart.java
+++ src/main/java/com/koped/model/Cart.java
@@ -1,8 +1,12 @@
 package com.koped.model;
+import lombok.Getter;
+import lombok.Setter;
+@Getter @Setter
 
 public class Cart {
-    private int id;
     private User user;
     private Product product;
     private int quantity;
+    private int id; // ini auto increment buat tiap object
+    private int price;
 }
