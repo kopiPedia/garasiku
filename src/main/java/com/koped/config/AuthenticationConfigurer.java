55224e46c2ec2d02ca05cf67aeec441ece6602d0
AdamMoh <xlkartu12345@gmail.com>
2024-04-21 20:39:59 +0700

    fix error in AuthenticationConfigurer

--- src/main/java/com/koped/config/AuthenticationConfigurer.java
+++ src/main/java/com/koped/config/AuthenticationConfigurer.java
@@ -1,8 +1,8 @@
 package com.koped.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 //@Configuration
 public class AuthenticationConfigurer {
