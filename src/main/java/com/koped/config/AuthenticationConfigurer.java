77bcf38418aafa9d70899ebb2313ec1030b982ff
AdamMoh <xlkartu12345@gmail.com>
2024-04-21 22:30:03 +0700

    fix sonar cloud test

--- src/main/java/com/koped/config/AuthenticationConfigurer.java
+++ src/main/java/com/koped/config/AuthenticationConfigurer.java
@@ -1,6 +1,6 @@
 package com.koped.config;
 
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
