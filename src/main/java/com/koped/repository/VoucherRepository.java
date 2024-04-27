815f2475bc1d4b362b76b2c93935daf0978a5c90
OmarK2804 <khalifomar4@gmail.com>
2024-04-27 17:49:54 +0700

    Update Voucher Repository

--- src/main/java/com/koped/repository/VoucherRepository.java
+++ src/main/java/com/koped/repository/VoucherRepository.java
@@ -2,32 +2,14 @@ package com.koped.repository;
 
 import com.koped.model.Voucher;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.ArrayList;
 import java.util.List;
-
-import java.util.Iterator;
 import java.util.UUID;
 
 @Repository
-public class VoucherRepository {
-    private List<Voucher> voucherData = new ArrayList<>();
-
-    public Voucher create(Voucher voucher) {
-        return voucher;
-    }
-
-    public Iterator<Voucher> findAll() { return voucherData.iterator();}
-
-    public Voucher findById(String id){
-        return null;
-    }
-
-    public Voucher update(String id, Voucher updatedVoucher){
-        return null;
-    }
-
-    public void delete(String id){
-
-    }
+public interface VoucherRepository extends JpaRepository<Voucher, Integer>{
+    Voucher findById(String id);
+    String delete(String id);
 }
