8d4e72c42c41ba59e383137cfdd20af87678956b
Obin Mandalika <obinnmandalikaa@gmail.com>
2024-05-09 16:43:35 +0700

    Implement Order Service

---
+++ src/main/java/com/koped/service/OrderService.java
@@ -0,0 +1,17 @@
+package com.koped.service;
+
+import com.koped.model.Order;
+import java.util.List;
+
+public interface OrderService {
+    Order createOrder(Order order);
+
+    Order updateStatus(int id, String status);
+
+    String deleteOrder(int id);
+
+    Order getOrderById(int id);
+
+    List<Order> getAllOrders();
+}
+
