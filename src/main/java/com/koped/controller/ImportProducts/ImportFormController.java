7d30f61ff30d078ffdfead8eb0c0f82379edb68a
Revaldyhfz <revaldyhafiz16@gmail.com>
2024-05-14 18:38:37 +0700

    Import Form Front end base, Create

--- src/main/java/com/koped/controller/ImportProducts/ImportFormController.java
+++ src/main/java/com/koped/controller/ImportProducts/ImportFormController.java
@@ -1,4 +1,4 @@
-package com.koped.controller.importform;
+package com.koped.controller.ImportProducts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -6,7 +6,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.koped.model.ImportForm;
+import com.koped.model.ImportProduct;
 import com.koped.service.ImportProductFormService;
+import com.koped.service.ImportProductService;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -16,13 +19,20 @@ public class ImportFormController {
     @Autowired
     private ImportProductFormService importProductFormService;
 
+    @Autowired
+    private ImportProductService importProductService;
+
     @GetMapping("/importform")
     public String importFormsPage(Model model) {
         // Assume userId is retrieved from session or authentication
-        int userId = 2; // Dummy userId for now, replace with actual userId retrieval logic
+        int userId = 2; // Dummy userId for now
 
         List<ImportForm> importForms = importProductFormService.findAllByUserId(userId);
+        List<ImportProduct> importProducts = importProductService.findAllProducts(); // Assuming you fetch all products
+
         model.addAttribute("importForms", importForms);
-        return "ImportForm"; // Assuming you have an import-forms.html template
+        model.addAttribute("importProducts", importProducts);
+        return "import-form";
     }
+
 }
