a032c59ce7ce519f58e3fb5f19e49f5178a9d4dd
ardhika23 <ardhika@gmail.com>
2024-05-23 18:03:20 +0700

    applied csrf token and beautify cart page

--- src/main/java/com/koped/controller/cart/CartController.java
+++ src/main/java/com/koped/controller/cart/CartController.java
@@ -1,20 +1,26 @@
 package com.koped.controller.cart;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
 import com.koped.model.Cart;
 import com.koped.service.CartService;
 import java.util.List;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 @Controller
 public class CartController {
     @Autowired
     private CartService cartService;
     @GetMapping("/cart")
     public String cartPage(Model model) {
-        List <Cart> cart = cartService.findCartByUser("john_doe");
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        List <Cart> cart = cartService.findCartByUser(username);
         model.addAttribute("cart", cart);
-        model.addAttribute("total", cartService.getPriceCart("john_doe"));
+        model.addAttribute("total", cartService.getPriceCart(username));
         return "cart";
     }
 }
