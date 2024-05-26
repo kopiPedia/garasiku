5951dc6022826fb0ebce3d929f2945ebe3f5b9a5
OmarK2804 <khalifomar4@gmail.com>
2024-05-26 23:47:34 +0700

    update payment

--- src/main/java/com/koped/controller/payment/PaymentController.java
+++ src/main/java/com/koped/controller/payment/PaymentController.java
@@ -1,7 +1,10 @@
 package com.koped.controller.payment;
+
 import com.koped.model.Cart;
+import com.koped.model.Product;
 import com.koped.model.Voucher;
 import com.koped.service.CartService;
+import com.koped.service.ProductService;
 import com.koped.service.VoucherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -22,15 +25,27 @@ public class PaymentController {
     @Autowired
     private VoucherService voucherService;
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("")
     public String paymentPage(Model model){
         String username = SecurityContextHolder.getContext().getAuthentication().getName();
-
         List<Cart> cart = cartService.findCartByUser(username);
+
+        double totalPrice = 0.0;
+        for (Cart item : cart) {
+            Product product = productService.findByProductIds(item.getProductId());
+            if (product != null) {
+                totalPrice += product.getPrice() * item.getQuantity();
+            }
+        }
+
         List<Voucher> voucherList = voucherService.findAllVoucher();
         model.addAttribute("vouchers", voucherList);
         model.addAttribute("cart", cart);
+        model.addAttribute("totalPrice", totalPrice);
+
         return "Payment/payment";
     }
-
 }
