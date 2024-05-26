b1cb8473f29e0fb0d7131c27fd2226096379e2ec
Revaldyhfz <revaldyhafiz16@gmail.com>
2024-05-26 10:30:59 +0700

    Fixed Creation

--- src/main/java/com/koped/service/ImportProductFormService.java
+++ src/main/java/com/koped/service/ImportProductFormService.java
@@ -2,9 +2,7 @@ package com.koped.service;
 
 import com.koped.model.ImportForm;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 public interface ImportProductFormService {
@@ -14,6 +12,5 @@ public interface ImportProductFormService {
     ResponseEntity<String> deleteByRequestId(String requestId);
     ResponseEntity<ImportForm> updateByRequestIds(String requestId, ImportForm data);
     ResponseEntity<ImportForm> createNewRequests(ImportForm data);
-    ResponseEntity<ImportForm> createNewRequests(ImportForm data, MultipartFile image) throws IOException;
     List<ImportForm> findAllByUserId(int userId);
 }
